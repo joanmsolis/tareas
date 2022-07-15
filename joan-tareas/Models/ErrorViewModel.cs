56554c7571c03d32eb6a6030abe78983b55eac3d
joanmsolis <j.m.s.l.19@hotmail.com>
2022-07-15 09:45:07 -0400

    creando mi lista de tarea en asp.net MVC

---
+++ joan-tareas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace joan_tareas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ joan-tareas/Models/TablaJoan01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace joan_tareas.Models
+{
+    public partial class TablaJoan01
+    {
+        public long Id { get; set; }
+        public string Nombre { get; set; }
+        public string Estado { get; set; }
+    }
+}
